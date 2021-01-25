000dcb819ce1511f9901bba67d8406b9f0b1e3c1
rahulkammili-git <kammilirahul@outlook.com>
2021-01-26 00:59:28 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,29 @@
 variable "prefix" {
+  default     = "Dev"
   description = "A prefix used for all resources in this example"
 }
+
+variable client_id {}
+
+variable client_secret {}
 
 variable "location" {
   default     = "West Europe"
   description = "The Azure Region in which all resources in this example should be provisioned"
 }
 
-variable "client_app_id" {
-  description = "The Client app ID of the AKS client application"
-}
+#variable "client_app_id" {
+ # description = "The Client app ID of the AKS client application"
+#}
 
-variable "server_app_id" {
-  description = "The Server app ID of  the AKS server application"
-}
+#variable "server_app_id" {
+ # description = "The Server app ID of  the AKS server application"
+#}
 
-variable "server_app_secret" {
-  description = "The secret created for AKS server application"
-}
+#variable "server_app_secret" {
+ # description = "The secret created for AKS server application"
+#}
 
-variable "tenant_id" {
-  description = "The Azure AD tenant id "
-}
+#variable "tenant_id" {
+#  description = "The Azure AD tenant id "
+#}
